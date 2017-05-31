4670383f9f9095edc1a1773802403dded827f0be
Ryan Smythe <ryan.smythe@gmail.com>
2017-05-31 07:48:53 -0400

    moving around arduino software trying to make the code clearer introduced SimpleTimer.h to try and automatically disable the channels after a certain time

--- v1-deprecated/software/wifire16/wifire16.ino
+++ v1-deprecated/software/wifire16/wifire16.ino
@@ -1,4 +1,5 @@
 #include <SPI.h>
+#include "lib\SimpleTimer.h"
 
 // pin definitions
 #define CLEARPIN 4    // master clear for 74HC595 shift registers
@@ -8,9 +9,10 @@
 #define DATAPIN  11   // data for 74HC595 shift registers
 #define CLOCKPIN 13   // clock for 74HC595 shift registers 
 
-#define bitFlip(x,n)  bitRead(x,n) ? bitClear(x,n) : bitSet(x,n)
+// the timer object
+SimpleTimer timer;
 
-char c;
+char serialIn;
 byte r1 = 0, r2 = 0;
 
 // setup
@@ -29,8 +31,8 @@ void setup() {
   // clear any lines that were left active
   digitalWrite(LATCHPIN, LOW);
   digitalWrite(OEPIN, HIGH);
-  c = SPI.transfer(0);
-  c = SPI.transfer(0);
+  SPI.transfer(0);
+  SPI.transfer(0);
   digitalWrite(LATCHPIN, HIGH);
   digitalWrite(OEPIN, LOW);
   
@@ -38,46 +40,66 @@ void setup() {
   digitalWrite(ARMEDPIN, HIGH);
 
   // start the serial communication with the xbee
-  Serial.begin(38400);
-
+  Serial.begin(115200);
 }
 
 
 // main loop
-void loop() {
+void loop() 
+{
+    if(Serial.available()) 
+    {
+        c = Serial.read();
 
-  if(Serial.available()) {
-    c = Serial.read();
-    switch(c) {
-      case '0' : bitFlip(r1,0); break;
-      case '1' : bitFlip(r1,1); break;
-      case '2' : bitFlip(r1,2); break;
-      case '3' : bitFlip(r1,3); break;
-      case '4' : bitFlip(r1,4); break;
-      case '5' : bitFlip(r1,5); break;
-      case '6' : bitFlip(r1,6); break;
-      case '7' : bitFlip(r1,7); break;
-      case '8' : bitFlip(r2,0); break;
-      case '9' : bitFlip(r2,1); break;
-      case 'a' : bitFlip(r2,2); break;
-      case 'b' : bitFlip(r2,3); break;
-      case 'c' : bitFlip(r2,4); break;
-      case 'd' : bitFlip(r2,5); break;
-      case 'e' : bitFlip(r2,6); break;
-      case 'f' : bitFlip(r2,7); break;
-      case '?' : 
-        // load power on = low, off = high
-        digitalRead(ARMEDPIN) ? Serial.print("-") : Serial.print("+");
-        break;  
+        if( (c >= 0) && (c <= 0x0f) ) 
+        {
+            activateChannel();
+        }
+        else if(c == '?')
+        {
+            // load power on = low, off = high
+            digitalRead(ARMEDPIN) ? Serial.print("-") : Serial.print("+");
+        }
     }
+
+    timer.run();
+}
+
+void activateChannel(byte channel)
+{
+    if(channel > 7) 
+    {
+      bitSet(r2,channel-8);
+    }
+    else 
+    {
+      bitSet(r1,channel);
+    }
+}
+
+void deactivateChannel(byte channel)
+{
+    if(c > 7) 
+    {
+      bitClear(r2,c-8);
+    }
+    else 
+    {
+      bitClear(r1,c);
+    }
+}
+
+void setTimer(byte r, byte channel)
+{
+    timer.setTimeout(200, deactivateChannel);
+}
+
+void transmit()
+{
     digitalWrite(LATCHPIN, LOW);
     digitalWrite(OEPIN, HIGH);
     c = SPI.transfer(r1);
     c = SPI.transfer(r2);
     digitalWrite(LATCHPIN, HIGH);
-    digitalWrite(OEPIN, LOW);
-  }
-  
+    digitalWrite(OEPIN, LOW);    
 }
-
-
